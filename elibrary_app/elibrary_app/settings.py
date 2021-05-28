8385fd340df987e50c0ca67c0c4e82892da57fe8
Kaique Luz <kakaluzrda@gmail.com>
2021-05-27 22:35:47 -0300

    commit css

--- elibrary_app/elibrary_app/settings.py
+++ elibrary_app/elibrary_app/settings.py
@@ -121,12 +121,18 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# arquivos staticos
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/img/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 ]
 
 # Simplified static file serving.
